e98470ab254c4160b6836b6d4a15e430ace3c442
Charlie Roth <charlieroth4@icloud.com>
2023-11-26 15:13:15 +0100

    Intiailize Store context, gen Product entity

--- lib/amazin_web/router.ex
+++ lib/amazin_web/router.ex
@@ -18,20 +18,15 @@ defmodule AmazinWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+
+    live "/products", ProductLive.Index, :index
+    live "/products/new", ProductLive.Index, :new
+    live "/products/:id/edit", ProductLive.Index, :edit
+    live "/products/:id", ProductLive.Show, :show
+    live "/products/:id/show/edit", ProductLive.Show, :edit
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", AmazinWeb do
-  #   pipe_through :api
-  # end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:amazin, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
