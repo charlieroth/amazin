8c2fcfe5e0bc33544f6f8444facaadf9e2e0d802
Charlie Roth <charlieroth4@icloud.com>
2023-11-26 16:18:14 +0100

    Add checkout event handler

--- lib/amazin_web/live/cart_live/show.ex
+++ lib/amazin_web/live/cart_live/show.ex
@@ -26,4 +26,37 @@ defmodule AmazinWeb.CartLive.Show do
   def handle_params(_params, _url, socket) do
     {:noreply, socket}
   end
+
+  @impl true
+  def handle_event("checkout", _params, socket) do
+    cart_items = Store.list_cart_items(socket.assigns.cart_id)
+
+    line_items =
+      cart_items
+      |> Enum.map(fn ci ->
+        %{
+          price_data: %{
+            currency: "usd",
+            product_data: %{
+              name: ci.product.name,
+              description: ci.product.description,
+              images: [ci.product.thumbnail]
+            },
+            unit_amount: ci.product.amount,
+            quantity: ci.quantity
+          }
+        }
+      end)
+
+    {:ok, checkout_session} =
+      Stripe.Checkout.Session.create(%{
+        line_items: line_items,
+        mode: :payment,
+        success_url: url(~p"/cart/success"),
+        cancel_url: url(~p"/cart"),
+        metadata: %{"cart_id" => socket.assigns.cart_id}
+      })
+
+    {:noreply, redirect(socket, external: checkout_session.url)}
+  end
 end
