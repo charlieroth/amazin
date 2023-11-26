2acaefc72e04fd538d38cfbc7900dfa84adffa3b
Charlie Roth <charlieroth4@icloud.com>
2023-11-26 15:01:40 +0100

    Add deps

--- config/config.exs
+++ config/config.exs
@@ -14,7 +14,7 @@ config :amazin,
 # Configures the endpoint
 config :amazin, AmazinWeb.Endpoint,
   url: [host: "localhost"],
-  adapter: Phoenix.Endpoint.Cowboy2Adapter,
+  adapter: Bandit.PhoenixAdapter,
   render_errors: [
     formats: [html: AmazinWeb.ErrorHTML, json: AmazinWeb.ErrorJSON],
     layout: false
--- mix.exs
+++ mix.exs
@@ -13,9 +13,6 @@ defmodule Amazin.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {Amazin.Application, []},
@@ -23,13 +20,9 @@ defmodule Amazin.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:phoenix, "~> 1.7.10"},
@@ -50,16 +43,12 @@ defmodule Amazin.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:bandit, "~> 1.1"},
+      {:money, "~> 1.12"},
+      {:stripity_stripe, "~> 3.1"}
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to install project dependencies and perform other setup tasks, run:
-  #
-  #     $ mix setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
